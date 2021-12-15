41b0abe9dbf852dc622c679af48f3bcf1b88c151
Bernard Thompson <bethomps@redhat.com>
2021-12-15 15:30:30 +0000

    refactor: put social icons into array of objects

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -3,39 +3,43 @@ import { FacebookLogo, TwitterLogo, InstagramLogo } from "phosphor-react";
 import styles from "../Main/Main.module.css";
 
 const Social = () => {
+  const icons = [
+    {
+      id: 0,
+      href: "https://www.facebook.com/barefootfarmdunhill",
+      aria: "Barefoot farm facebook url",
+      iconLogo: <FacebookLogo className={styles.iconSize} color='#4e3c31' />,
+    },
+    {
+      id: 1,
+      href: "https://twitter.com/barefootfarm_du",
+      aria: "Barefoot farm twitter url",
+      iconLogo: <TwitterLogo className={styles.iconSize} color='#4e3c31' />,
+    },
+    {
+      id: 2,
+      href: "https://www.instagram.com/barefoot.farm/",
+      aria: "Barefoot farm instagram url",
+      iconLogo: <InstagramLogo className={styles.iconSize} color='#4e3c31' />,
+    },
+  ];
   return (
     <div className={styles.wrapper}>
       <div className={styles.contactWrapper}>
-        <div className={styles.socialIcon}>
-          <a
-            href='https://www.facebook.com/barefootfarmdunhill'
-            target='_blank'
-            rel='noreferrer'
-            aria-label='Barefoot farm facebook url'
-          >
-            <FacebookLogo className={styles.iconSize} color='#4e3c31' />
-          </a>
-        </div>
-        <div className={styles.socialIcon}>
-          <a
-            href='https://twitter.com/barefootfarm_du'
-            target='_blank'
-            rel='noreferrer'
-            aria-label='Barefoot farm twitter url'
-          >
-            <TwitterLogo className={styles.iconSize} color='#4e3c31' />
-          </a>
-        </div>
-        <div className={styles.socialIcon}>
-          <a
-            href='https://www.instagram.com/barefoot.farm/'
-            target='_blank'
-            rel='noreferrer'
-            aria-label='Barefoot farm instagram url'
-          >
-            <InstagramLogo className={styles.iconSize} color='#4e3c31' />
-          </a>
-        </div>
+        {icons.map((i) => {
+          return (
+            <div className={styles.socialIcon} key={i.id}>
+              <a
+                href={i.href}
+                target='_blank'
+                rel='noreferrer'
+                aria-label={i.aria}
+              >
+                {i.iconLogo}
+              </a>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
